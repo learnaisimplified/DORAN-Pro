3acd24b8dd620930d9bb6ae2b69b7df0ec3c9a4a
learnaisimplified <learnaisimplified@gmail.com>
2025-07-21 18:42:44 +0800

    Update get.ps1

--- get.ps1
+++ get.ps1
@@ -1,5 +1,5 @@
-# Download and run the compiled DORAN.exe from GitHub
-$exeUrl = "https://raw.githubusercontent.com/learnaisimplified/DORAN-Pro/main/dist/private/DORAN.exe"
+# Download and run the compiled DORAN.exe from GitHub Releases
+$exeUrl = "https://github.com/learnaisimplified/DORAN-Pro/releases/download/v1.2.3/DORAN.exe"
 $tempExe = "$env:TEMP\DORAN.exe"
 
 Invoke-WebRequest -Uri $exeUrl -OutFile $tempExe -UseBasicParsing
