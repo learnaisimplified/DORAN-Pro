b1c3e8f7ddb7a7e45cf7eb39f7bf75471581e475
learnaisimplified <learnaisimplified@gmail.com>
2025-07-21 21:01:26 +0800

    Update get.ps1

--- get.ps1
+++ get.ps1
@@ -1,6 +1,13 @@
-# Download and run the compiled DORAN.exe from GitHub Releases
+# Define download location
 $exeUrl = "https://github.com/learnaisimplified/DORAN-Pro/releases/download/v1.2.3/DORAN.exe"
 $tempExe = "$env:TEMP\DORAN.exe"
 
+# Kill any running DORAN.exe (ignore errors)
+Get-Process DORAN -ErrorAction SilentlyContinue | Stop-Process -Force -ErrorAction SilentlyContinue
+
+# Wait a bit for file to be released
+Start-Sleep -Milliseconds 500
+
+# Download and run
 Invoke-WebRequest -Uri $exeUrl -OutFile $tempExe -UseBasicParsing
 Start-Process -FilePath $tempExe -WindowStyle Hidden
